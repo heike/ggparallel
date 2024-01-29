36d6e8fd7bee4f513fb7e549c4c6835dedc4f15d
Heike Hofmann <hofmann@iastate.edu>
2024-01-29 08:32:09 -0600

    test for hammock plot

--- tests/testthat/test-ggparallel.R
+++ tests/testthat/test-ggparallel.R
@@ -14,4 +14,19 @@ test_that("ggparallel works", {
   # we have the right layer classes
   expect_contains(classes, c("GeomRibbon", "GeomBar", "GeomText"))
 
+  # throw error
+  expect_message(expect_error(ggparallel(list("gear"), data=mtcars)))
+})
+
+test_that("ggparallel hammocks work", {
+  load(system.file("titanic_hammock.rda", package="ggparallel")) # loads object titanic_hammock
+
+  titanic <- as.data.frame(Titanic)
+  titanic_plot <- ggparallel(names(titanic)[c(1,4,2,3)], titanic, weight=1, asp=0.5,
+                        method="hammock", ratio=0.2, order=c(0,0))
+  build_now <- ggplot2::ggplot_build(titanic_plot)
+# titanic_hammock <- build_now$data
+# save(titanic_hammock, file="inst/titanic_hammock.rda")
+
+  expect_identical(titanic_hammock, build_now$data)
 })
