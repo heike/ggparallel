e51132fdb863fab88171190522b85f0c884ac236
Heike Hofmann <hofmann@iastate.edu>
2024-01-29 09:00:53 -0600

    set larger tolerance

--- tests/testthat/test-ggparallel.R
+++ tests/testthat/test-ggparallel.R
@@ -1,3 +1,12 @@
+save_png <- function(code, width = 400, height = 400) {
+  path <- tempfile(fileext = ".png")
+  png(path, width = width, height = height)
+  on.exit(dev.off())
+  code
+
+  path
+}
+
 test_that("ggparallel works", {
   load(system.file("mtcars.rda", package="ggparallel")) # loads object test_mtcars_plot
   built_saved <- ggplot2::ggplot_build(test_mtcars_plot)
@@ -23,10 +32,11 @@ test_that("ggparallel hammocks work", {
 
   titanic <- as.data.frame(Titanic)
   titanic_plot <- ggparallel(names(titanic)[c(1,4,2,3)], titanic, weight=1, asp=0.5,
-                        method="hammock", ratio=0.2, order=c(0,0))
+                             method="hammock", ratio=0.2, order=c(0,0))
   build_now <- ggplot2::ggplot_build(titanic_plot)
-# titanic_hammock <- build_now$data
-# save(titanic_hammock, file="inst/titanic_hammock.rda")
+  # titanic_hammock <- build_now$data
+  # save(titanic_hammock, file="inst/titanic_hammock.rda")
 
-  expect_identical(titanic_hammock, build_now$data)
+  expect_equal(titanic_hammock, build_now$data, tolerance=1e-4)
 })
+
